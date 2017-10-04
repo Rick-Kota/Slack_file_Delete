cc6756e163c38c7481292783a4d971ca7948383e
Oz-paul <j7w1.taro@gmail.com>
2017-10-04 20:48:47 +0900

    TEMP_SAVE

--- slack.py
+++ slack.py
@@ -4,16 +4,16 @@ import time
 import calendar
 from datetime import datetime, timedelta
 
-_token = "xxxx"
-_domain = "xxxxx"
+_token = "xxxxxxx"
+_domain = "xxxxxx"
 
 def del_time(Day):
-    Set_time = Day*24*60*60
+    Set_time = str(int(time.time())-Day*86400)
     return Set_time
 
 def files_list(Day):
-    Del_time = (time.time()) - del_time(Day)
-    files_list_url = "https://slack.com/api/files.list?token=" + _token +"&pretty=1"
+    Del_time = del_time(Day)
+    files_list_url = "https://slack.com/api/files.list"
     data = {"token": _token, "ts_to": Del_time, "count":1000}
     response = requests.post(files_list_url,data)
     return response.json()["files"]
@@ -31,10 +31,12 @@ if __name__ == '__main__':
         for f in files:
             print ("Deleting file " + f["name"] + "...")
             timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
-            delete_url = "https://" + _domain + ".slack.com/api/files.delete?t=" + timestamp
-            requests.post(delete_url, data = {
-              "token": _token,
-              "file": f["id"],
-              "set_active": "true",
-              "_attempts": "1"})
+            delete_url = "https://slack.com/api/files.delete"
+            data = {
+                    "token": _token,
+                    "file": f["id"],
+                    "set_active": "true",
+                    "_attempts": "1"
+                    }
+            requests.post(delete_url, data)
 print ("DONE!")
